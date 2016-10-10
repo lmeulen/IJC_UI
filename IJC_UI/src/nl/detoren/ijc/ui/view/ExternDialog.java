287eeddada38a53c5a3efcae9c9500176f39b238
lmeulen <leo.vander.meulen@gmail.com>
2016-10-10 21:36:02 +0200

    Update licentie naar GPL 3.0

--- IJC_UI/src/nl/detoren/ijc/ui/view/ExternDialog.java
+++ IJC_UI/src/nl/detoren/ijc/ui/view/ExternDialog.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (C) 2016 Leo van der Meulen
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * See: http://www.gnu.org/licenses/gpl-3.0.html
+ *  
+ * Problemen in deze code:
+ * - ...
+ */
 package nl.detoren.ijc.ui.view;
 
 import java.awt.Dialog;
