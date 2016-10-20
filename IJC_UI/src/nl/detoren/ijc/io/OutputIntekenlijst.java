d47f92b1e2e208f9d15ace1354acb7e34d40bfa5
lmeulen <leo.vander.meulen@gmail.com>
2016-10-20 21:38:27 +0200

    Intekenlijst niet laten eindigen met lege pagina

--- IJC_UI/src/nl/detoren/ijc/io/OutputIntekenlijst.java
+++ IJC_UI/src/nl/detoren/ijc/io/OutputIntekenlijst.java
@@ -39,7 +39,8 @@ public class OutputIntekenlijst {
 	private final static Logger logger = Logger.getLogger(OutputIntekenlijst.class.getName());
 
 	public void exportIntekenlijst(Groepen uitslag) {
-		if (!IJCController.c().exportIntekenlijst) return;
+		if (!IJCController.c().exportIntekenlijst)
+			return;
 		try {
 			logger.log(Level.INFO, "Wedstrijden wegschrijven naar Excel");
 			FileInputStream file = new FileInputStream("Leeg.docx");
@@ -51,6 +52,8 @@ public class OutputIntekenlijst {
 			run.setFontSize(12);
 			String result;
 			for (int i = 0; i < uitslag.getAantalGroepen(); ++i) {
+				if (i >= 1)
+					run.addBreak();
 				Groep groep = uitslag.getGroepById(i);
 				result = "Stand na " + uitslag.getRonde() + "e ronde, " + uitslag.getPeriode();
 				result += "e periode                " + groep.getNaam() + " (" + groep.getSpelers().size() + ")\n";
@@ -62,12 +65,11 @@ public class OutputIntekenlijst {
 				result = "-----------------------------------------------------------------------\n";
 				run.setText(result);
 				run.addBreak();
-		        for (Speler s : groep.getSpelers()) {
-		            result = s.toPrintableString(false);
+				for (Speler s : groep.getSpelers()) {
+					result = s.toPrintableString(false);
 					run.setText(result);
 					run.addBreak();
-		        }
-
+				}
 
 				if (IJCController.c().exportDoorschuivers) {
 					int ndoor = IJCController.c().bepaalAantalDoorschuivers(uitslag.getPeriode(), uitslag.getRonde());
@@ -81,19 +83,18 @@ public class OutputIntekenlijst {
 							result = s.toPrintableString(false) + "\n";
 							run.setText(result);
 							run.addBreak();
-}
+						}
 						result = IJCController.c().exportDoorschuiversStop + "\n" + "\n";
 						run.setText(result);
-						run.addBreak();
 					}
 				}
-				run.addCarriageReturn();                 //separate previous text from break
+				run.addCarriageReturn(); // separate previous text from break
 				run.addBreak(BreakType.PAGE);
 			}
 			// Close input file
 			file.close();
 			// Store Excel to new file
-			String outputFile = "IntekenlijstR" + uitslag.getPeriode()+ "-" + uitslag.getRonde()+ ".docx";
+			String outputFile = "IntekenlijstR" + uitslag.getPeriode() + "-" + uitslag.getRonde() + ".docx";
 			FileOutputStream outFile = new FileOutputStream(new File(outputFile));
 			document.write(outFile);
 			// Close output file
@@ -108,15 +109,17 @@ public class OutputIntekenlijst {
 
 	/**
 	 * Set spacing to double
+	 * 
 	 * @param para
 	 */
 	public void setDoubleLineSpacing(XWPFParagraph para) {
-	    CTPPr ppr = para.getCTP().getPPr();
-	    if (ppr == null) ppr = para.getCTP().addNewPPr();
-	    CTSpacing spacing = ppr.isSetSpacing()? ppr.getSpacing() : ppr.addNewSpacing();
-	    spacing.setAfter(BigInteger.valueOf(0));
-	    spacing.setBefore(BigInteger.valueOf(0));
-	    spacing.setLineRule(STLineSpacingRule.AUTO);
-	    spacing.setLine(BigInteger.valueOf(480));
+		CTPPr ppr = para.getCTP().getPPr();
+		if (ppr == null)
+			ppr = para.getCTP().addNewPPr();
+		CTSpacing spacing = ppr.isSetSpacing() ? ppr.getSpacing() : ppr.addNewSpacing();
+		spacing.setAfter(BigInteger.valueOf(0));
+		spacing.setBefore(BigInteger.valueOf(0));
+		spacing.setLineRule(STLineSpacingRule.AUTO);
+		spacing.setLine(BigInteger.valueOf(480));
 	}
 }
